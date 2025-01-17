c4edbf6c7c8f3733ef03f385e13a29cffdd1813a
020226 <mmin08@naver.com>
2025-01-17 09:54:29 +0900

    세팅

---
+++ src/main/java/com/sbs/exam/qdsl/boundedContext/user/entity/SiteUser.java
@@ -0,0 +1,24 @@
+package com.sbs.exam.qdsl.boundedContext.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+public class SiteUser {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(unique = true)
+  private String username;
+
+  private String password;
+
+  @Column(unique = true)
+  private String email;
+}
